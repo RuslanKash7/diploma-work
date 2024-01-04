79448ae740c6fded68bd95220c79d1ae03ee8d76
ReNe Kash <dantess88@mail.ru>
2024-01-04 22:44:02 +0500

    условный рендеринг формы добавления отзыва

--- client/src/components/ui/comments.jsx
+++ client/src/components/ui/comments.jsx
@@ -8,7 +8,8 @@ import {
   loadCommentsList,
   removeComment,
 } from "../../store/comments";
-import { getIsLoggedIn } from "../../store/users";
+import { getUserById, getIsLoggedIn } from "../../store/users";
+import localStorageService from "../../services/localStorage.service";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 
@@ -19,11 +20,30 @@ const Comments = () => {
     dispatch(loadCommentsList(productId));
   }, [dispatch, productId]);
 
+  const currentUserId = localStorageService.getUserId();
+  const theUser = useSelector(getUserById(currentUserId));
+
   const isLoading = useSelector(getCommentsLoadingStatus());
 
   const comments = useSelector(getComments());
   const isAuth = useSelector(getIsLoggedIn());
-  console.log(isAuth);
+
+  const isBought = (theUser, productId) => {
+    // Перебираем массив purchase
+    for (const purchase of theUser.purchase) {
+      // Перебираем массив userCart в каждом элементе purchase
+      for (const item of purchase.userCart) {
+        // Проверяем наличие targetProductId
+        if (item.productId === productId) {
+          return true; // Нашли productId, возвращаем true
+        }
+      }
+    }
+    // Если productId не найден
+    return false;
+  };
+
+  console.log(isBought(theUser, productId));
 
   const handleSubmit = (data) => {
     dispatch(createComment({ ...data, productId: productId }));
@@ -37,12 +57,14 @@ const Comments = () => {
 
   return (
     <>
-      {isAuth && (
+      {isAuth && isBought(theUser, productId) ? (
         <div className="card mb-2">
           <div className="card-body ">
             <AddCommentForm onSubmit={handleSubmit} />
           </div>
         </div>
+      ) : (
+        "Вы сможите добавить отзыв после оформления покупки этого товара"
       )}
       {sortedComments.length > 0 && (
         <div className="card mb-3">
